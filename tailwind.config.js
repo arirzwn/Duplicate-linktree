bea0594e1bd4657497fe0788a5ec8fda9f8f6c03
arirzwn <aririzwan0@gmail.com>
2023-01-31 09:09:58 +0700

    Upload source code

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        warna1: "#6F59EA",
+        warna2: "#E6B2C8",
+      },
+    },
+  },
+
+  plugins: [],
+};
